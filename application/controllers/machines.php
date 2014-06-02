6228ccd03de982bd4bff5ec7ea722cb339520ccf
romdo <roman.d@brilliant-solutions.eu>
2014-06-02 11:13:53 +0300

    fixing apache AllowOverride

---
+++ application/controllers/machines.php
@@ -0,0 +1,22 @@
+<?php if (!defined('BASEPATH')) {
+  exit('No direct script access allowed');
+}
+
+class Machines extends CI_Controller {
+
+  public function index() {
+    echo 'machines';
+  }
+
+  function add() {
+    echo 'add';
+  }
+
+  function edit() {
+    echo 'edit';
+  }
+
+  function delete() {
+    echo 'delete';
+  }
+}
