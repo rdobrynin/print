455cc0e894cd5562be005bf77a0d7bddd164827c
romdo <roman.d@brilliant-solutions.eu>
2014-06-16 09:08:15 +0300

    added ajax controller

---
+++ application/controllers/ajax.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) {
+  exit('No direct script access allowed');
+}
+
+class Ajax extends CI_Controller {
+
+
+// check email
+  public function check_email() {
+
+
+  }
+
+}
