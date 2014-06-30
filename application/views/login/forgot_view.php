3d58af603d19d2ae14583ad53991b6800436091e
romdo <roman.d@brilliant-solutions.eu>
2014-06-30 11:43:55 +0300

    removed bootstrap

--- application/views/login/forgot_view.php
+++ application/views/login/forgot_view.php
@@ -1,30 +1,10 @@
-<?php include('login/head_login.php');?>
-
 <body>
-
-<div>
   <div class="account-container">
     <div class="content clearfix">
     <!--  HELPER FORM-->
-
-
       <h1>Forgot password</h1>
-
-
       <div class="sign_up"><a href="<?php echo base_url();?>">Log in</a></div>
     </div> <!-- /content -->
     <div id="check_login"></div>
-
-  </div> <!-- /account-container -->
-  <!-- JavaScript -->
-  <script src="js/jquery-1.10.2.js"></script>
-  <script src="js/bootstrap.js"></script>
-  <!-- Page Specific Plugins -->
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-  <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
-  <script src="js/morris/chart-data-morris.js"></script>
-  <script src="js/tablesorter/jquery.tablesorter.js"></script>
-  <script src="js/tablesorter/tables.js"></script>
-  <script src="js/script.js"></script>
 </body>
 </html>
--- application/views/login/signup_view.php
+++ application/views/login/signup_view.php
@@ -1,4 +1,4 @@
-<?php include('login/head_login.php');?>
+<?php include('head.php');?>
 
 <body>
 
@@ -63,15 +63,5 @@
     <div id="check_login"></div>
 
   </div> <!-- /account-container -->
-  <!-- JavaScript -->
-  <script src="js/jquery-1.10.2.js"></script>
-  <script src="js/bootstrap.js"></script>
-  <!-- Page Specific Plugins -->
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-  <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
-  <script src="js/morris/chart-data-morris.js"></script>
-  <script src="js/tablesorter/jquery.tablesorter.js"></script>
-  <script src="js/tablesorter/tables.js"></script>
-  <script src="js/script.js"></script>
 </body>
 </html>
