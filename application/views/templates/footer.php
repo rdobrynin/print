5593fdf9bacf0d252abc04cd7fad1520a762b0b1
romdo <roman.d@brilliant-solutions.eu>
2014-07-02 16:25:29 +0300

    fixed form users

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,6 +3,7 @@
 <script src="js/jquery-1.10.2.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/script.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
 
 <!-- Custom JavaScript for the Menu Toggle -->
 <script>
--- application/views/templates/profile_view.php
+++ application/views/templates/profile_view.php
@@ -5,53 +5,42 @@
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
         <h4 class="modal-title custom_align" id="Heading">Profile</h4>
       </div>
+      <form role="form">
       <div class="modal-body">
-          <div class="row">
-                  <div class="row">
-                    <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=100" class="img-circle">
-                      <br>
-                      <br>
-                      <div class=""><span class=" badge badge-warning">8 messages</span></div></div>
-
-                    <div class=" col-md-9 col-lg-9 ">
-                      <table class="table table-user-information">
-                        <tbody>
-                        <tr>
-                          <td>Name:</td>
-                          <td><?php print($user[0]['first_name']." " .$user[0]['last_name']);?></td>
-                          <td><span class="glyphicon glyphicon-pencil"></span></td>
-                        </tr>
-                        <tr>
-                          <td>Created:</td>
-                          <td><?php print($user[0]['date_created'])?></td>
-                        </tr>
-                        <tr>
-                          <td>Position</td>
-                          <td><?php print(show_role($user[0]['role']));?></td>
-                          <td><span class="glyphicon glyphicon-pencil"></span></td>
-                        </tr>
-                        <tr>
-                        <tr>
-                          <td>E-mail</td>
-                          <td><a href="mailto:<?php print($user[0]['email_address'])?>"><?php print($user[0]['email_address'])?></a></td>
-                          <td><span class="glyphicon glyphicon-pencil"></span></td>
-                        </tr>
-                        <tr>
-                          <td>Phone number:</td>
-                          <td>+372 551213242</td>
-                          <td><span class="glyphicon glyphicon-pencil"></span></td>
-                        </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-              </div>
+     <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="first_name">First name</label>
+            <input type="text" value="<?php print($user[0]['first_name']);?>" class="form-control" id="first_name" placeholder="First name">
+          </div>
         </div>
+       <div class="col-md-6">
+         <div class="form-group">
+           <label for="last_name">Last name</label>
+           <input type="text"  value="<?php print($user[0]['last_name']);?>" class="form-control" id="last_name" placeholder="Last name">
+         </div>
+       </div>
+         <div class="col-md-4">
+           <div class="form-group">
+             <label for="first_name">Role</label>
+           <select class="form-control" id="role">
+             <option value="<?php print($user[0]['role']);?>"><?php print(show_role($user[0]['role']));?></option>
+             <option value="2">Manager</option>
+             <option value="3">Implementor</option>
+             <option value="4">Customer</option>
+             <option value="5">Observer</option>
+             </select>
+           </div>
+         </div>
+
+     </div>
+
       <div class="modal-footer ">
         <span class="pull-right"><a href="#" data-dismiss="modal" class="btn btn-default">Close</a></span>
-        <span class="pull-left"><a href="#"  class="btn btn-info">Update</a></span>
+        <button type="submit" class="btn btn-default pull-left">Submit</button>
       </div>
       </div>
+      </form>
     </div>
   </div>
 </div>
