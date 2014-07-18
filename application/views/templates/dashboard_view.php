9a9d1a7d788038b6409e804f32b89b2f4ba2e7ed
romdo <roman.d@brilliant-solutions.eu>
2014-07-18 09:21:18 +0300

    rformde code

--- application/views/templates/dashboard_view.php
+++ application/views/templates/dashboard_view.php
@@ -4,48 +4,7 @@
     <div id="page-content-wrapper">
         <div class="page-content inset">
     <div class="row">
-        <div class="col-md-8">asas</div>
-        <div class="col-md-4">
-            <p class="lead">Last comment</p>
-            <div class="[ panel panel-default ] panel-google-plus">
-                <div class="dropdown">
-                    <span class="dropdown-toggle" type="button" data-toggle="dropdown">
-                        <span class="[ glyphicon glyphicon-chevron-down ]"></span>
-                    </span>
-                    <ul class="dropdown-menu" role="menu">
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                        <li role="presentation" class="divider"></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-                    </ul>
-                </div>
-                <div class="panel-heading">
-                    <img class="[ img-circle pull-left ]" src="https://lh3.googleusercontent.com/-CxXg7_7ylq4/AAAAAAAAAAI/AAAAAAAAAQ8/LhCIKQC5Aq4/s46-c-k-no/photo.jpg" alt="Mouse0270" />
-                    <h3>Robert McIntosh</h3>
-                    <h5><span>Shared publicly</span> - <span>Jun 27, 2014</span> </h5>
-                </div>
-                <div class="panel-body">
-                    <p>Do people born in 2000 get to choose if they are Generation Y or Generation Z? How do you decide what generation you want to belong to?</p>
-                </div>
-                <div class="panel-footer">
-                    <button type="button" class="[ btn btn-default ]">+1</button>
-                    <button type="button" class="[ btn btn-default ]">
-                        <span class="[ glyphicon glyphicon-share-alt ]"></span>
-                    </button>
-                    <div class="input-placeholder">Add a comment...</div>
-                </div>
-                <div class="panel-google-plus-comment">
-                    <img class="img-circle" src="<?php base_url();?>uploads/avatar/bg_man@2x.jpg" width="40" height="40" alt="User Image" />
-                    <div class="panel-google-plus-textarea">
-                        <textarea rows="4"></textarea>
-                        <button type="submit" class="btn btn-success btn-xs disabled">Post comment</button>
-                        <button type="reset" class="btn btn-xs btn-default">Cancel</button>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </div>
+
     </div>
             </div>
         </div>
