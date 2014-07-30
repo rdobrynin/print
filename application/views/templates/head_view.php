ec5959ac403ed598419c86698d99e3c302435ead
romdo <roman.d@brilliant-solutions.eu>
2014-07-30 18:25:00 +0300

    fixed nav

--- application/views/templates/head_view.php
+++ application/views/templates/head_view.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Brilliant project management</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="<?php print(base_url());?>css/bootstrap.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/bootstrap-switch.min.css" rel="stylesheet">
-
-  <!-- Add custom CSS here -->
-  <link href="<?php print(base_url());?>css/sidebar.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/add_client.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/users.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/mini_inbox.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/body.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/elements.css" rel="stylesheet">
-    <link href="<?php print(base_url());?>css/dashboard.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/responsive.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/task_table.css" rel="stylesheet">
-  <link href="<?php print(base_url());?>css/help_block.css" rel="stylesheet">
-    <link href="<?php print(base_url());?>css/right_sidebar.css" rel="stylesheet">
-    <link href="<?php print(base_url());?>css/modal.css" rel="stylesheet">
-    <link href="<?php print(base_url());?>css/bootstrap-select.css" rel="stylesheet">
-    <link href="<?php print(base_url());?>css/btn.css" rel="stylesheet">
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Brilliant project management</title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php print(base_url()); ?>css/bootstrap.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/bootstrap-switch.min.css" rel="stylesheet">
+    <!-- Add custom CSS here -->
+    <link href="<?php print(base_url()); ?>css/sidebar.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/add_client.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/users.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/mini_inbox.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/body.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/top.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/elements.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/dashboard.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/responsive.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/task_table.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/help_block.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/right_sidebar.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/modal.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/bootstrap-select.css" rel="stylesheet">
+    <link href="<?php print(base_url()); ?>css/btn.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
 </head>
-
 <body>
 <div class="right-sidebar">
-
-
 </div>
