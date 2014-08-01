81d0eff65e5f6229a65bf5d6d880b509375f9867
romdo <roman.d@brilliant-solutions.eu>
2014-08-01 11:27:47 +0300

    fixed database config

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'roman';
-$db['default']['password'] = 'roman123';
-$db['default']['database'] = 'roman';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'qwerty';
+$db['default']['database'] = 'prm';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
@@ -63,6 +63,23 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+
+//$db['default']['hostname'] = 'localhost';
+//$db['default']['username'] = 'roman';
+//$db['default']['password'] = 'roman123';
+//$db['default']['database'] = 'roman';
+//$db['default']['dbdriver'] = 'mysql';
+//$db['default']['dbprefix'] = '';
+//$db['default']['pconnect'] = TRUE;
+//$db['default']['db_debug'] = TRUE;
+//$db['default']['cache_on'] = FALSE;
+//$db['default']['cachedir'] = '';
+//$db['default']['char_set'] = 'utf8';
+//$db['default']['dbcollat'] = 'utf8_general_ci';
+//$db['default']['swap_pre'] = '';
+//$db['default']['autoinit'] = TRUE;
+//$db['default']['stricton'] = FALSE;
+
 
 
 /* End of file database.php */
--- application/language/estonian/estonian_lang.php
+++ application/language/estonian/estonian_lang.php
@@ -9,3 +9,47 @@ $lang['menu_comments'] = "Kommentaar";
 $lang['menu_help'] = "Järele aitama";
 $lang['menu_lang'] = "Keel";
 $lang['menu_add_client'] = "Lisa klient";
+$lang['menu_settings'] = "Settings";
+$lang['menu_profile'] = "Profile";
+$lang['menu_admin_users'] = "Administer users";
+$lang['menu_logout'] = "Log out";
+$lang['menu_messages'] = "Messages";
+$lang['menu_invite_person'] ='Invite person';
+$lang['mess_new_messages'] ='New messages';
+$lang['mess_inbox'] ='Inbox';
+$lang['mess_view_inbox'] ='View inbox';
+
+$lang['profile_view'] ='Profile view';
+$lang['req_info'] ='Requirement information';
+$lang['first_name'] ='First name';
+$lang['last_name'] ='Last name';
+$lang['add_phone'] ='Add phone';
+$lang['phone'] ='Phone';
+$lang['prim_email'] ='Primary email';
+$lang['add_email'] ='Add email';
+$lang['role'] ='Role';
+$lang['add_info'] ='Additional information';
+$lang['profile_picture'] ='Avatar';
+
+
+
+$lang['back'] ='Back';
+$lang['submit'] ='Submit';
+$lang['upload'] ='Upload';
+$lang['remove'] ='Remove';
+
+$lang['company'] ='Company';
+$lang['company_desc'] ='Company description';
+$lang['url'] ='URL';
+$lang['regional_info'] ='Regional information';
+$lang['address'] ='address';
+$lang['index'] ='index';
+$lang['country'] ='Country';
+$lang['city'] ='City';
+
+$lang['role_master'] ='Master';
+$lang['role_curator'] ='Curator';
+$lang['role_implementor'] ='Implementor';
+$lang['role_customer'] ='Customer';
+$lang['role_guest'] ='Guest';
+$lang['role_manager'] ='Manager';
