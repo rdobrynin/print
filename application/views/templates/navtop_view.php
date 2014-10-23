bdc573114d158e16c0169728d59130c7c95a9060
romdo <roman.d@brilliant-solutions.eu>
2014-10-23 17:12:55 +0300

    fixed navbar

--- application/views/templates/navtop_view.php
+++ application/views/templates/navtop_view.php
@@ -67,9 +67,9 @@
         <li><a href="<?php print(base_url());?>profile"><i class="fa fa-user"></i><?php print(lang('menu_profile'))?></a></li>
         <li><a href="#"><i class="fa fa-envelope"></i><?php print(lang('mess_inbox'))?><span class="badge badge-resp">7</span></a></li>
         <li><a data-toggle="modal" href="#settings"><i class="fa fa-gear"></i><?php print(lang('menu_settings'))?></a></li>
-        <?php if($user[0]['role']==5):?>
-          <li><a href="<?php print(base_url());?>users"><i class="fa fa-users"></i><?php print(lang('menu_admin_users'))?></a></li>
-        <? endif?>
+<!--        --><?php //if($user[0]['role']==5):?>
+<!--          <li><a href="--><?php //print(base_url());?><!--users"><i class="fa fa-users"></i>--><?php //print(lang('menu_admin_users'))?><!--</a></li>-->
+<!--        --><?// endif?>
         <li class="divider"></li>
         <li id="logout"><a href="<?php print(base_url());?>logout"><i class="fa fa-power-off"></i><?php print(lang('menu_logout'))?></a></li>
       </ul>
