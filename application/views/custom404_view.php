0a4c3ccfc9cf0084c4df37d308bbbeb2b1716971
rdobrynin <roman.dobrynin@gmail.com>
2014-12-09 22:42:56 +0200

    fixed error message

--- application/views/custom404_view.php
+++ application/views/custom404_view.php
@@ -15,7 +15,7 @@
 
 </head>
 <body>
-<div class="row">
+<div class="container">
   <div class="page-404">
     <img src="<?php print(base_url());?>img/page-404.png" alt="">
     <h3>We're sorry, but the page you were looking for doesn't exist.</h3>
@@ -36,7 +36,7 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                 <h3>We'd Love to Hear From You</h3>
             </div>
-            <div class="modal-body">
+            <div class="modal-body" style="padding-bottom: 0">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
@@ -55,6 +55,7 @@
                             <label for="error_email">Email</label>
                             <input type="text" name="error_email" id="error_email" class="form-control btn-special" placeholder="Your email">
                         </div>
+                        <div style="display: none; margin-bottom: 10px; padding: 10px; font-size: 12px;" id="error_email_empty_modal" class="label label-danger label-signin"><i class="fa fa-exclamation-circle"></i>&nbsp;Email address incorrect</div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
@@ -63,10 +64,11 @@
                         </div>
                     </div>
                 </div>
-                <div style="display: none; margin-bottom: 10px;" id="error_success_modal" class="label label-primary label-signin"><i class="fa fa-exclamation-circle"></i>&nbsp;Your message has been successfully sended</div>
+                <div style="display: none; margin-bottom: 10px; padding: 10px; font-size: 12px;" id="error_success_modal" class="label label-primary label-signin"><i class="fa fa-exclamation-circle"></i>&nbsp;Your message has been successfully sent</div>
+                <div style="display: none; margin-bottom: 10px; padding: 10px; font-size: 12px;" id="error_empty_modal" class="label label-danger label-signin"><i class="fa fa-exclamation-circle"></i>&nbsp;All fields must be filled</div>
             </div>
-            <div class="modal-footer">
-                <button type="button" style="width: 100%;" class="btn btn-success pull-left">Send It!</button>
+            <div class="modal-footer" style="padding-top: 0">
+                <button type="button" style="width: 100%;" id="error_send_btn" class="btn btn-success pull-left">Send It!</button>
             </div>
         </div>
         <?php form_close( );?>
@@ -77,6 +79,51 @@
 <script src="<?php print(base_url());?>assets/js/jquery-1.10.2.js"></script>
 <script src="<?php print(base_url());?>assets/js/bootstrap.min.js"></script>
 <script src="<?php print(base_url());?>js/script.js"></script>
+
+<script>
+    $("#error_send_btn").click(function () {
+        var form_data = {
+            name: $('#error_title').val(),
+            message: $('#error_message').val(),
+            email: $('#error_email').val(),
+            phone: $('#error_phone').val()
+
+        };
+        $.ajax({
+            url: "<?php echo site_url('ajax/error_mail'); ?>",
+            type: 'POST',
+            data: form_data,
+            dataType: 'json',
+            success: function (msg) {
+                console.log(msg);
+                if(msg.empty == false) {
+                    $('#error_empty_modal').css('display', 'block');
+                }
+                if(msg.empty == true) {
+                    $('#error_empty_modal').css('display', 'none');
+                }
+
+                if(msg.check_email == false) {
+                    $('#error_email_empty_modal').css('display', 'block');
+                }
+                else {
+                    $('#error_email_empty_modal').css('display', 'none');
+                }
+
+                if(msg.send == true) {
+                    $('#error_success_modal').css('display', 'block');
+                    setTimeout(function() {
+                        $("input[type=text], textarea").val("");
+                        $('#error_success_modal').css('display', 'none');
+                        $('#error_modal').modal('hide');
+                    }, 3000);
+                }
+            }
+        });
+
+    });
+</script>
+
 </body>
 
 </html>
